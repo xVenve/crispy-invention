1bf7102f35a89ee6d974506f28019873e1f37919
CarlosCRO <carlosrubiolivares2000@gmail.com>
2021-09-16 16:40:24 +0200

    RNA Practice 1 1.1 Creadas las dos primeras funciones

--- RNA/Practice 1/processData.py
+++ RNA/Practice 1/processData.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 df = pd.read_csv('concrete.csv')
 
-df_norm = (df - df.min()) / (df.max() - df.min())
-df_norm = df_norm.sample(frac=1)
-df_norm.to_csv('concreteProcessed.csv', index=False)
+df_norm = (df - df.min()) / (df.max() - df.min()) # Normaliza la entrada
+df_norm = df_norm.sample(frac=1) # Aleatoriza las instancias
+df_norm.to_csv('concreteProcessed.csv', index=False) # Guardamos el fichero normalizado y aleatorizado
 
-_7 = df_norm.sample(frac=0.7)
-_3 = df_norm.drop(_7.index)
-result = np.array_split(_3, 2)
+_7 = df_norm.sample(frac=0.7) # Obtiene el 70% de las entradas
+_3 = df_norm.drop(_7.index) # Obtiene el 30% de las entradas
+result = np.array_split(_3, 2) # Del 30% anterior, los divide en 2 partes, del 15% de las entradas cada una
 
+# Almacena los datos anteriores en cada fichero
 _7.to_csv('training.csv', index=False)
 result[0].to_csv('validation.csv', index=False)
 result[1].to_csv('test.csv', index=False)
