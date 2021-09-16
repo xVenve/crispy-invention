415c12e5a4633e6baaaf53b4d37c64b48b049d30
Jorge Rodríguez <jrf1616@gmail.com>
2021-09-16 21:55:06 +0200

    RNA Practice 1 1.1.2

--- RNA/Practice 1/main.py
+++ RNA/Practice 1/main.py
@@ -1,23 +1,21 @@
-import pandas
 import numpy
 import csv
 
-def createData():
-    data = numpy.array(list(csv.reader(open(r"C:\Users\Carlos\github\crispy-invention\RNA\Practice 1\test.csv", "rt"), delimiter=","))).astype("float")
-    weights = numpy.random.uniform(-1, 1, 9) # Podemos cambiar el rango por -5, +5
-    print(weights)
 
+def createData():
+    data = numpy.array(list(csv.reader(open(r".\test.csv", "rt"), delimiter=","))).astype("float")
+    weights = numpy.random.uniform(-1, 1, 9)  # Podemos cambiar el rango por -5, +5
     return data, weights
+
 
 def createOutput(pattern, weights):
     pattern[8] = 1.0
-    a = 0
     return numpy.dot(pattern, weights)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     d, w = createData()
 
-    for i in range (0, 1000):
-        for j in range (0, len(d)):
+    for i in range(0, 1000):
+        for j in range(0, len(d)):
             createOutput(d[j], w)
--- RNA/Practice 1/processData.py
+++ RNA/Practice 1/processData.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('concrete.csv')
+df = pd.read_csv("concrete.csv")
 
-df_norm = (df - df.min()) / (df.max() - df.min()) # Normaliza la entrada
-df_norm = df_norm.sample(frac=1) # Aleatoriza las instancias
-df_norm.to_csv('concreteProcessed.csv', index=False) # Guardamos el fichero normalizado y aleatorizado
+df_norm = (df - df.min()) / (df.max() - df.min())  # Normaliza la entrada
+df_norm = df_norm.sample(frac=1)  # Aleatoriza las instancias
+df_norm.to_csv("concreteProcessed.csv", index=False)  # Guardamos el fichero normalizado y aleatorizado
 
-_7 = df_norm.sample(frac=0.7) # Obtiene el 70% de las entradas
-_3 = df_norm.drop(_7.index) # Obtiene el 30% de las entradas
-result = np.array_split(_3, 2) # Del 30% anterior, los divide en 2 partes, del 15% de las entradas cada una
+_7 = df_norm.sample(frac=0.7)  # Obtiene el 70 % de las entradas
+_3 = df_norm.drop(_7.index)  # Obtiene el 30 % de las entradas
+result = np.array_split(_3, 2)  # Del 30 % anterior, los divide en 2 partes, del 15 % de las entradas cada una
 
 # Almacena los datos anteriores en cada fichero
-_7.to_csv('training.csv', index=False)
-result[0].to_csv('validation.csv', index=False)
-result[1].to_csv('test.csv', index=False)
+_7.to_csv("training.csv", header=False, index=False)
+result[0].to_csv("validation.csv", header=False, index=False)
+result[1].to_csv("test.csv", header=False, index=False)
