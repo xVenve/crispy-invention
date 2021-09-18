ca7470c3a3cbe797432674803f3fa56c8e472129
Jorge Rodríguez <jrf1616@gmail.com>
2021-09-18 23:57:19 +0200

    AGE Practice 1 1.2
    Se ha comenzado la memoria y se ha almacenado la salida del programa
    para posibles futuras comparaciones

--- AGE/Practice 1/brute-force.py
+++ AGE/Practice 1/brute-force.py
@@ -8,22 +8,25 @@ min_eva = None  # Value of min evaluation
 min_chromosome = None  # Chromosome that produce de min
 nEval = 0  # Num. of evaluations
 min_neva = 0  # Num. of evaluations of min_eva
-output_file = open('output_brute-force.txt', 'a')
+output_file = open('output_brute-force.txt', 'w')
+# all_output_file = open('all_output_file.txt', 'w')
+# all_output_best_fitness_file = open('all_output_best_fitness_file.txt', 'w')
+
 
 for i in range(0, pow(2, 16)):
     chromosome_binary = chromosome_seed
+    request_answer = None
 
     chromosome_int = int(chromosome_binary, 2) + pow(2, 3 * 16) * i  # Adjust for the station 1
     chromosome_binary = format(chromosome_int, "064b")
-
     try:
-        r = requests.get(web_to_request + chromosome_binary)
+        request_answer = requests.get(web_to_request + chromosome_binary)
         nEval = nEval + 1
     except:
         i = i - 1
 
-    if min_eva is None or min_eva > r.text:
-        min_eva = r.text
+    if min_eva is None or min_eva > request_answer.text:
+        min_eva = request_answer.text
         min_chromosome = chromosome_binary
         min_neva = nEval
         newMin = str(min_neva) + ", " + min_chromosome + ", " + min_eva
@@ -31,7 +34,13 @@ for i in range(0, pow(2, 16)):
         print(newMin)
 
     # print(str(nEval) + ", " + chromosome_binary + ", " + r.text)
+    # all_output = str(nEval) + ", " + chromosome_binary + ", " + r.text
+    # all_output_best_fitness = str(nEval) + ", " + min_chromosome + ", " + min_eva
+    # all_output_file.write(all_output + "\n")
+    # all_output_best_fitness_file.write(all_output_best_fitness + "\n")
 
 print("Se han realizado", nEval, "evaluaciones, el mejor resultado ha sido", min_eva, "para el cromosoma",
-      min_chromosome, "se ha obtenido en la evaluación", min_eva)
+      min_chromosome, "se ha obtenido en la evaluación", min_neva)
 output_file.close()
+# all_output_file.close()
+# all_output_best_fitness_file.close()
