96bfa35db0c1e80a0c6d8259ff17dedc3fa58fab
Jorge Rodríguez <jrf1616@gmail.com>
2021-10-08 00:06:33 +0200

    RNA Practice 1 1.8
    Primero modelos para la segunda parte

---
+++ RNA/Practice 1/PM2021_reg.R
@@ -0,0 +1,121 @@
+library(RSNNS)
+
+graficaError <- function(iterativeErrors){
+        plot(1:nrow(iterativeErrors),iterativeErrors[,1], type="l", main="Evoluci?n del error",
+             ylab="MSE",xlab="Ciclos",
+             ylim=c(min(iterativeErrors),max(iterativeErrors)))
+        lines(1:nrow(iterativeErrors),iterativeErrors[,2], col="red")
+}
+
+
+## funcion que calcula el error cuadratico medio
+MSE <- function(pred,obs) {sum((pred-obs)^2)/length(obs)}
+
+
+#CARGA DE DATOS
+# se supone que los ficheros tienen encabezados
+# si no los tienen, cambiar header a F
+ trainSet <- read.csv("training.csv",dec=".",sep=",",header = T)
+ validSet <- read.csv("validation.csv",dec=".",sep=",",header = T)
+ testSet  <- read.csv("test.csv",dec=".",sep=",",header = T)
+
+
+
+salida <- ncol (trainSet)   #num de la columna de salida
+
+
+
+
+
+#SELECCION DE LOS PARAMETROS
+topologia        <- c(60) #PARAMETRO DEL TIPO c(A,B,C,...,X) A SIENDO LAS NEURONAS EN LA CAPA OCULTA 1, B LA CAPA 2 ...
+razonAprendizaje <- 0.1 #NUMERO REAL ENTRE 0 y 1
+ciclosMaximos    <- 100000 #NUMERO ENTERO MAYOR QUE 0
+
+#EJECUCION DEL APRENDIZAJE Y GENERACION DEL MODELO
+
+set.seed(1)
+model <- mlp(x= trainSet[,-salida],
+             y= trainSet[, salida],
+             inputsTest=  validSet[,-salida],
+             targetsTest= validSet[, salida],
+             size= topologia,
+             maxit=ciclosMaximos,
+             learnFuncParams=c(razonAprendizaje),
+             shufflePatterns = F
+             )
+
+#GRAFICO DE LA EVOLUCION DEL ERROR
+#plotIterativeError(model)
+
+# lo desactivamos porque a veces tiene problemas con las escalas. 
+# establece el m?nimo y el m?ximo en funci?n del error de entrenamiento y a veces
+# no muestra el de validaci?n si es m?s peque?o.
+#Usamos graficaError() pero antes hay que calcular iterativeErrors que contiene
+#los errores MSE por ciclo de train y valid 
+
+# DATAFRAME CON LOS ERRORES POR CICLo: de entrenamiento y de validacion
+iterativeErrors <- data.frame(MSETrain= (model$IterativeFitError/ nrow(trainSet)),
+                              MSEValid= (model$IterativeTestError/nrow(validSet)))
+
+graficaError(iterativeErrors)
+
+
+######################################################################
+#SE OBTIENE EL NuMERO DE CICLOS DONDE EL ERROR DE VALIDACION ES MINIMO
+#######################################################################
+
+nuevosCiclos <- which.min(model$IterativeTestError)
+
+#ENTRENAMOS LA MISMA RED CON LAS ITERACIONES QUE GENERAN MENOR ERROR DE VALIDACION
+set.seed(1)
+model <- mlp(x= trainSet[,-salida],
+             y= trainSet[, salida],
+             inputsTest=  validSet[,-salida],
+             targetsTest= validSet[, salida],
+             size= topologia,
+             maxit=nuevosCiclos,
+             learnFuncParams=c(razonAprendizaje),
+             shufflePatterns = F
+)
+#GRAFICO DE LA EVOLUCION DEL ERROR
+#plotIterativeError(model)
+
+iterativeErrors1 <- data.frame(MSETrain= (model$IterativeFitError/ nrow(trainSet)),
+                              MSEValid= (model$IterativeTestError/nrow(validSet)))
+graficaError(iterativeErrors1)
+
+#CALCULO DE PREDICCIONES
+prediccionesTrain <- predict(model,trainSet[,-salida])
+prediccionesValid <- predict(model,validSet[,-salida])
+prediccionesTest  <- predict(model, testSet[,-salida])
+
+#CALCULO DE LOS ERRORES
+errors <- c(TrainMSE= MSE(pred= prediccionesTrain,obs= trainSet[,salida]),
+            ValidMSE= MSE(pred= prediccionesValid,obs= validSet[,salida]),
+            TestMSE=  MSE(pred= prediccionesTest ,obs= testSet[,salida]))
+errors
+
+
+
+
+
+#SALIDAS DE LA RED
+outputsTrain <- data.frame(pred= prediccionesTrain,obs= trainSet[,salida])
+outputsValid <- data.frame(pred= prediccionesValid,obs= validSet[,salida])
+outputsTest  <- data.frame(pred= prediccionesTest, obs=  testSet[,salida])
+
+
+
+
+#GUARDANDO RESULTADOS
+saveRDS(model,"nnet.rds")
+write.csv2(errors,"finalErrors.csv")
+write.csv2(iterativeErrors,"iterativeErrors.csv")
+write.csv2(outputsTrain,"netOutputsTrain.csv")
+write.csv2(outputsValid,"netOutputsValid.csv")
+write.csv2(outputsTest, "netOutputsTest.csv")
+
+
+
+
